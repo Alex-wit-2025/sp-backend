d59e1fa7dafbf55e9b5b0f5b37c1ca2389983429
Alex <17982529+alexm622@users.noreply.github.com>
2025-07-24 20:04:26 -0400

    added host option

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ async function main() {
   console.log("About to start Fastify server..."); // Add this line
 
   try {
-    await fastify.listen({ port: 3000 });
+    await fastify.listen({ port: 3000 , host: "0.0.0.0"});
     console.log("🚀 Server listening on http://localhost:3000");
   } catch (err) {
     fastify.log.error(err);
